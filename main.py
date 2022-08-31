b6f8929389e72a8c20d94317de650baf8cd796db
mr2space <princegoswami.space@gmail.com>
2022-08-31 08:17:04 +0530

    added number to grid

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ response = requests.get("https://sugoku.herokuapp.com/board?difficulty=easy")
 grid = response.json()['board']
 grid_original = []
 grid_original.extend(grid) 
-
+print(grid)
 # Intialize the pygame
 
 pygame.init()
@@ -22,7 +22,8 @@ pygame.display.set_caption("Sudoku Awesome Game")
 icon = pygame.image.load(r"Include\resources\restaurant.png")
 pygame.display.set_icon(icon)
 
-
+# font and color
+font = pygame.font.SysFont('Agency FB',30)
 
 def quitGame():
     for event in pygame.event.get():
@@ -40,10 +41,18 @@ def sudokuGrid():
         width = 1
 
 
+def displayGameNumber():
+    for i in range(9):
+        for j in range(9):
+            number = grid[i][j]
+            if 0 < number < 10:
+                value = font.render(str(grid[i][j]),True,(255,0,0))
+                screen.blit(value, (70*j+70, 70*i+60))
 
 running = True
 while running:
     running = quitGame()
     screen.fill((255,255,255))
     sudokuGrid()
+    displayGameNumber()
     pygame.display.update()
