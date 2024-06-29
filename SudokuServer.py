1cf5818bc712f0309293996e7cfc9b7abc68dac7
Prince Goswami <prince@mrprincegoswami.in>
2024-06-29 08:43:34 +0530

    foldered

--- SudokuServer.py
+++ SudokuServer.py
@@ -4,14 +4,14 @@ import os
 import sys
 from _thread import *
 import pickle
-import Components.Components as Components
+from GameMode import OnlineGameMode
 import sudokuGen
 
 server = ""
 port = 5555
 connectionCount = 0
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-playerMap:dict[int, list[Components.OnlineGameMode, Components.OnlineGameMode]] = {}
+player_map:dict[int, list[OnlineGameMode,OnlineGameMode]] = {}
 game_id = 0
 player_id = -1
 
@@ -25,12 +25,12 @@ except Exception as err:
 s.listen()
 
 def connection_thread(conn, connectionCount, game_id , player_id):
-    conn.send(pickle.dumps(playerMap[game_id][player_id]))
+    conn.send(pickle.dumps(player_map[game_id][player_id]))
     while True:
         try:
-            flag = sudokuGen.checkGameStatus(playerMap[game_id][player_id].grid)
+            flag = sudokuGen.checkGameStatus(player_map[game_id][player_id].grid)
             # player[connectionCount] = pickle.loads(conn.recv(4096))
-            conn.sendall(pickle.dumps(player[(connectionCount+1)%2]))
+            conn.sendall(pickle.dumps(player_map[(connectionCount+1)%2]))
         except Exception as err:
             print("error in thread function",err)
             exc_type, exc_obj, exc_tb = sys.exc_info()
@@ -38,16 +38,16 @@ def connection_thread(conn, connectionCount, game_id , player_id):
             print(exc_type, fname, exc_tb.tb_lineno)
             break
     print("connection lost")
-    del playerMap[game_id]
+    del player_map[game_id]
 
 while True:
     conn, addr = s.accept()
     print("connected to", addr)
     if player_id == 0:
         game_id += 1
-        game1 = Components.OnlineGameMode(None,game_id,0)
+        game1 = OnlineGameMode(None,game_id,0)
         game2 = copy.deepcopy(game1)
-        playerMap[game_id] = [game1, game2]
+        player_map[game_id] = [game1, game2]
         start_new_thread(connection_thread, (conn, connectionCount, game_id, player_id))
     else:
         start_new_thread(connection_thread, (conn, connectionCount, game_id, player_id))
